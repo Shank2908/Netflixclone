8ae3bc4b580b23e39d844ed0ef10264635ffe59d
Shankey singh <114279995+Shank2908@users.noreply.github.com>
2024-04-06 22:35:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+ document.getElementById("number").onclick = function mynumber() {
+    alert("https://www.netflix.com wants to open this application.\n Always allows www.netflix.com to open links of this type in the associated app");
+
+ };
+
+document.getElementById("language").onclick = function mylanguage() {
+   document.getElementById("language").style.border = "5px solid white";
+}
+   
+ 
+ 
+
